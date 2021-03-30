34b61ea7c441b6d1a2521403fa280d64c6595662
Walter Renzo Muñoz Arellano <wmunozarellano@gmail.com>
2021-03-30 10:39:39 -0500

    [FIX] Error in production

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,8 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
+  generate: {
+    fallback: true,
+  },
   head: {
     title: 'Mazda - Derco Perú',
     htmlAttrs: {
